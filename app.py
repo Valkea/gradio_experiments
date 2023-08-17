46f5b918b7e6d57680dfd5a8dca34b00326e550e
Emmanuel <github-sub@shedge.com>
2023-08-17 18:30:31 +0200

    refactor: add HF token to use PROTECTED endpoint
    refactor: black format

--- app.py
+++ app.py
@@ -6,12 +6,22 @@ import requests
 import gradio as gr
 from huggingface_hub import InferenceClient
 
-endpoint_url = os.getenv('API_URL')
 
-# Streaming Client
-client = InferenceClient(endpoint_url) # , token=hf_token)
+# --- GET KEYS ---
 
-# generation parameter
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+endpoint_url = os.getenv("API_URL")
+hf_token = os.getenv("HF_TOKEN")
+
+# --- DEFINE VARIABLES ---
+
+# -- streaming client
+client = InferenceClient(endpoint_url, token=hf_token)
+
+# -- generation parameter
 gen_kwargs = dict(
     max_new_tokens=256,
     top_k=30,
@@ -21,46 +31,51 @@ gen_kwargs = dict(
     stop_sequences=["###", "</s>"],
 )
 
+# --- DEFINE FUNCTIONS ---
+
 
 def create_prompt_formats(user_input, context=None):
-
-    INTRO_BLURB = "Below is an instruction that describes a task. Write a short response that appropriately completes the request. But avoid giving too many details."
+    INTRO_BLURB = "Below is an instruction that describes a task. Write a short response that appropriately completes the request. But avoid giving too much details."
     INSTRUCTION_KEY = "### Instruction:"
     INPUT_KEY = "Input:"
     RESPONSE_KEY = "### Response:"
 
-    blurb =         f"{INTRO_BLURB}"
-    instruction =   f"{INSTRUCTION_KEY}\n{user_input}"
+    blurb = f"{INTRO_BLURB}"
+    instruction = f"{INSTRUCTION_KEY}\n{user_input}"
     input_context = f"{INPUT_KEY}\n{context}" if context else None
-    response =      f"{RESPONSE_KEY}\n"
+    response = f"{RESPONSE_KEY}\n"
 
     parts = [part for part in [blurb, instruction, input_context, response] if part]
     formatted_prompt = "\n\n".join(parts)
     return formatted_prompt
 
+
 def predict(message, history):
 
-    # prompt
     prompt = create_prompt_formats(message, None)
-    
     stream = client.text_generation(prompt, stream=True, details=True, **gen_kwargs)
-
     answer = ""
-    
-    # yield each generated token
+
+    # -- yield each generated token
     for r in stream:
-        # skip special tokens
+
+        # -- skip special tokens
         if r.token.special:
             continue
-        # stop if we encounter a stop sequence
+
+        # -- stop if we encounter a stop sequence
         if r.token.text in gen_kwargs["stop_sequences"]:
             break
-        # yield the generated token
-        print(r.token.text, end = "")
+
+        # -- yield the generated token
+        print(r.token.text, end="")
         answer += r.token.text
         yield answer
 
+
+# --- DEFINE INTERFACE ---
+
 demo = gr.ChatInterface(predict)
-    
-# demo.launch()   
+
+# demo.launch()
 demo.queue(concurrency_count=75).launch(debug=True)
